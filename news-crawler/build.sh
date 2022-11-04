47c94321f8483e0e478a4e32f653c6edc878f86a
thanh5320 <thanh20183632@gmail.com>
2022-11-04 19:21:13 +0700

    fix deploy

--- news-crawler/build.sh
+++ news-crawler/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-docker build -t 192.168.1.6:5555/alert-crawler-news-post:v1.0.$1 .
-docker push 192.168.1.6:5555/alert-crawler-news-post:v1.0.$1
+docker build -t alert-crawler-news-post:v1.0.1 .
+#docker push 192.168.1.6:5555/alert-crawler-news-post:v1.0.$1
+docker rm -f alert-tc2-crawler-news-post
+docker run --name alert-tc2-crawler-news-post -v /etc/hosts:/etc/hosts:ro -d --restart unless-stopped alert-crawler-news-post:v1.0.1
